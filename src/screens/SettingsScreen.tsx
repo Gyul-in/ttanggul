340379198e50a469716489ceb9e331b2cf6f5fa1
Gyul_in_ <gyulinbog@gmail.com>
2026-05-19 19:40:48 +0900

    feat: 설정 화면 피그마 변경에 따른 리스트 구조 업데이트

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -241,15 +241,23 @@ export default function SettingsScreen({ navigation }: Props) {
                 }
               />
               <SettingItem label="알림 시간" value={notificationTime || '11:00 PM'} onPress={() => setTimeSheetVisible(true)} />
-              <SettingItem label="카테고리" value={preferredCategory?.replace('#', '') || '공감'} isLast onPress={() => setQuoteSheetVisible(true)} />
+              <SettingItem label="글귀" value={preferredCategory?.replace('#', '') || '공감'} isLast onPress={() => setQuoteSheetVisible(true)} />
+            </View>
+          </View>
+
+          <View style={styles.settingsGroup}>
+            <SubHeader title="법적정보" />
+            <View style={styles.listContainer}>
+              <SettingItem label="개인정보 처리방침" isFirst onPress={() => {}} />
+              <SettingItem label="이용 약관" onPress={() => {}} />
+              <SettingItem label="마케팅 정보 수신" isLast onPress={() => {}} />
             </View>
           </View>
 
           <View style={styles.settingsGroup}>
             <SubHeader title="기타" />
             <View style={styles.listContainer}>
-              <SettingItem label="개인정보처리방침" isFirst />
-              <SettingItem label="의견 보내기" onPress={() => setFeedbackSheetVisible(true)} />
+              <SettingItem label="의견 보내기" isFirst onPress={() => setFeedbackSheetVisible(true)} />
               <SettingItem label="버전정보" value="v1.0" valueColor="gray600" showArrow={false} isLast />
             </View>
           </View>
